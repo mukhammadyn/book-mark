ac7f99b7806b9426fc7f1a67d7733c935848a0ff
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2021-06-07 16:37:08 +0500

    Add feature tabs

--- script/main.js
+++ script/main.js
@@ -26,3 +26,31 @@ elsFaqButton.forEach(function (button) {
 });
 
 
+    var elsTabsItem = document.querySelectorAll('.features-navigation-item');
+    var elsTabsLink = document.querySelectorAll('.features-navigation__btn');
+    var elsPanel = document.querySelectorAll('.feature--item');
+
+    // Har bir link clickga quloq solsin. Har biriga link deb murojaat qilamiz. O'zimiz nom beramiz
+    elsTabsLink.forEach(function (link) {
+      // Har bir linkka click reaksiyasini beramiz
+      link.addEventListener('click', function (evt) {
+        // Linkning tabiiy reaksiyasi (sakrash)ning oldi olinadi
+        evt.preventDefault();
+
+        // Hamma li lardan active klassni olib tashlaymiz. Har biriga item deb murojaat qilamiz
+        elsTabsItem.forEach(function (item) {
+          item.classList.remove('feature--item__active');
+        });
+
+        // Bosilgan linkning otasiga active klassini qo'shamiz
+        link.parentElement.classList.add('feature--item__active');
+
+        // Hamma panellarni yopamiz
+        elsPanel.forEach(function (panel) {
+          panel.classList.remove('feature--item__active');
+        });
+
+        // Bog'langan panelni topamiz
+        document.querySelector(link.getAttribute('href')).classList.add('feature--item__active');
+      });
+    });
